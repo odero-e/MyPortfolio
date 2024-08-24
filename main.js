427ac62e7805f6fbbc9feb9cd1fb525f47ba5fb6
odero-e <garciabern0106@gmail.com>
2024-08-24 20:44:16 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
 /*===================== Toggle navbar ===================*/
-
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-	menuIcon.classList.toggle('fa-xmark');
-	navbar.classList.toggle('active');
-} 
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
 
-/*===================== scroll section active link ===================*/
-
+/*===================== Scroll section active link ===================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
-	sections.forEach(sec => {
-		let top = window.scrollY;
-		let offset = sec.offsetTop - 150;
-		let height = sec.offsetHeight;
-		let id = sec.getAttribute ('id');
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-		if (top >= offset && top < offset + height) {
-			navLinks.forEach.apply(links => {
-				links.classList.remove('active');
-				document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-			});
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
 
-		};
-	});
-}
+    /*===================== Sticky navbar ===================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*===================== Remove toggle icon and navbar ===================*/
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
